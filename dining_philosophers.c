b0e48d868f055c803f0d51798c163cb6a86280c8
Ram <ramhari.dahal1@gmail.com>
2018-02-24 22:12:30 -0700

    Eat and think

--- dining_philosophers.c
+++ dining_philosophers.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
 
 void think(int philo, int n) {
-
+	int sleep_time = rand() % n + 1;
+	sleep(sleep_time);
 }
 
 void pickup(int philo, int n) {
@@ -11,7 +13,8 @@ void pickup(int philo, int n) {
 }
 
 void eat(int philo, int n) {
-
+	int sleep_time = rand() %n + 1;
+	sleep(sleep_time);
 }
 
 void putdown(int philo, int n) {
@@ -28,8 +31,18 @@ void action(void *philo) {
 int main () {
 	// Take number of philosophers as input
 	int n;
+	printf("Enter number of philosophers: \n");
 	scanf("%d", &n);
 
+	pthread_mutex_t chopsticks(n);
+	pthread_t philosophers(n);
+	pthread_attr_t att(n);
+
+	// Create threads
+	int i;
+	for (i = 0; i < n; i++) {
+		pthread_mutex_init(&chopsticks[i], NULL);
+	}
 	
 
 	return 0;
