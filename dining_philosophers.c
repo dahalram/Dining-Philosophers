410323aa192d889807c1b96b33e799e13762677a
Ram <ramhari.dahal1@gmail.com>
2018-02-27 10:04:21 -0700

    Change n to global and putdown function

--- dining_philosophers.c
+++ dining_philosophers.c
@@ -3,22 +3,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-void think(int philo, int n) {
+int n;	// Make the number of philosophers as global variable
+pthread_mutex_t chopsticks[n];
+pthread_t philosophers[n];
+
+void think(int philo) {
 	int sleep_time = rand() % n + 1;
 	sleep(sleep_time);
 }
 
-void pickup(int philo, int n) {
+void pickup(int philo) {
 
 }
 
-void eat(int philo, int n) {
+void eat(int philo) {
 	int sleep_time = rand() %n + 1;
 	sleep(sleep_time);
 }
 
-void putdown(int philo, int n) {
-
+void putdown(int philo) {
+	pthread_mutex_unlock(&chopsticks[(philo +1) % n];
+	pthread_mutex_unlock(&chopsticks[philo % n]); 
 }
 
 void action(void *philo) {
@@ -33,10 +38,10 @@ int main () {
 	int n;
 	printf("Enter number of philosophers: \n");
 	scanf("%d", &n);
+	
+	srand(time(NULL));
 
-	pthread_mutex_t chopsticks(n);
-	pthread_t philosophers(n);
-	pthread_attr_t att(n);
+	//pthread_attr_t att(n);
 
 	// Create threads
 	int i;
@@ -44,6 +49,13 @@ int main () {
 		pthread_mutex_init(&chopsticks[i], NULL);
 	}
 	
+	for (i = 0; i < n; i++) {
+		pthread_create(&philosophers[i], NULL, action, i);
+	}
+	
+	for (i = 0; i < n; i++) {
+		pthread_join(philosophers[i], NULL);
+	}
 
 	return 0;
 }
